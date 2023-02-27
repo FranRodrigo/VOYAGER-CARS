a8579746d07063e8b58ff20f13d729591f2f224f
Fran Rodrigo <franjrodrigop@gmail.com>
2023-02-27 17:39:22 +0100

    models and associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,12 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+      :recoverable, :rememberable, :validatable
+  has_many :cars
+  has_many :bookings
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :address, presence: true
 end
