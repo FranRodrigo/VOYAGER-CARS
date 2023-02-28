7ba672a175d694f76f219b0e2abade2d719c9ba4
James Bunyon <bunyonjames@gmail.com>
2023-02-28 14:15:04 +0100

    cars seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Car.destroy_all
+
+john = User.create(first_name: "John", last_name: "Smith", email: "user1@aol.com", address: "Köln")
+
+puts "Creating Cars"
+bmw = { brand: "Bmw", model: "Serie 3", location: "Ruldorfplatz", number_plate: "Kaa7586", capacity: "5", year: "2018", price: "35", user: john }
+mercedes = { brand: "Mercedes", model: "A1", location: "Zulpicheplatz", number_plate: "Kbb0002", capacity: "4", year: "2020", price: "30", user: john }
+audi = { brand: "Audi", model: "A5", location: "Barbarossaplatz", number_plate: "Kcc0003", capacity: "4", year: "2017", price: "40",  user: john }
+volskwagen = { brand: "Volskwagen", model: "Golf", location: "Deutz", number_plate: "Kdd0003", capacity: "4", year: "2022", price: "25", user: john}
+
+[bmw, mercedes, audi, volskwagen].each do |attributes|
+  car = Car.create!(attributes)
+  puts "Created #{car.brand}"
+end
+puts "Finished!"
