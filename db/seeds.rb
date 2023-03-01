2bea7dd304f5dcaf8dc1c83c04de3682d68b00e5
James Bunyon <bunyonjames@gmail.com>
2023-03-01 15:09:50 +0100

    fixed the seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 require "open-uri"
+Booking.destroy_all
 Car.destroy_all
 
 john = User.create(first_name: "John", last_name: "Smith", email: "user1@aol.com", address: "Köln", password: "password")
