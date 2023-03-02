3514d98fbe9b97c7a6fe16ee91dcd0920bec2dd1
Lena Mueller <lenakristinmueller@gmail.com>
2023-03-02 16:19:25 +0100

    search bar

--- app/models/car.rb
+++ app/models/car.rb
@@ -4,4 +4,11 @@ class Car < ApplicationRecord
   has_many_attached :photos
 
   validates :brand, :model, :location, :number_plate, :capacity, :year, :price, presence: true
+
+  include PgSearch::Model
+  pg_search_scope :search_by_location,
+    against: [ :location ],
+    using: {
+      tsearch: { prefix: true }
+    }
 end
