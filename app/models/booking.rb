27250be7d32d66464367448eeced8080e52797e8
Fran Rodrigo <franjrodrigop@gmail.com>
2023-03-02 14:06:16 +0100

    validations date

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,25 @@
 class Booking < ApplicationRecord
   belongs_to :car
   belongs_to :user
+
+  validates :pickup_day, presence: true
+  validates :return_day, presence: true
+
+  validate :date_cannot_be_in_the_past
+
+  def date_cannot_be_in_the_past
+    return unless pickup_day.present? && return_day.present?
+
+    if pickup_day < Date.today
+      add_past_date_error(errors, :pickup_day)
+    end
+
+    if return_day < Date.today
+      add_past_date_error(errors, :return_day)
+    end
+  end
+
+  def add_past_date_error(errors, attribute)
+    errors.add(attribute, "can't be in the past")
+  end
 end
