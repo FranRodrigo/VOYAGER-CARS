64db38dfd1ae3a65ddd1a9890ae061b90c64df4b
Fran Rodrigo <franjrodrigop@gmail.com>
2023-03-02 15:54:15 +0100

    review

--- app/models/car.rb
+++ app/models/car.rb
@@ -2,6 +2,7 @@ class Car < ApplicationRecord
   belongs_to :user
   has_many :bookings
   has_many_attached :photos
+  has_many :reviews, dependent: :destroy
 
   validates :brand, :model, :location, :number_plate, :capacity, :year, :price, presence: true
 end
