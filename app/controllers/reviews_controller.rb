26311b7884bba4012e80464fac2db2e2d0c823fa
James Bunyon <bunyonjames@gmail.com>
2023-03-03 10:56:32 +0100

    review logic

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,14 +1,38 @@
 class ReviewsController < ApplicationController
+  before_action :authenticate_user!
+
   def create
     @car = Car.find(params[:car_id])
-    @review = Review.new(review_params)
-    @review.car = @car
+    @booking = current_user.bookings.find_by(car_id: @car.id)
+
+    if !@booking.present? || @car.owner?(current_user)
+      redirect_to @car, alert: 'You are not allowed to review this car (yet).'
+      return
+    end
+
+    if current_user.wrote_review_for?(@booking)
+      redirect_to @car, alert: 'You have already written a review for this car'
+    end
+
+    @review = @car.reviews.new(review_params)
+    @review.booking = @booking
+
     if @review.save
-      redirect_to car_path(@car)
+      redirect_to @car, notice: 'Review was successfully created.'
     else
       render "cars/show", status: :unprocessable_entity
     end
   end
+  # def create
+  #   @car = Car.find(params[:car_id])
+  #   @review = Review.new(review_params)
+  #   @review.car = @car
+  #   if @review.save
+  #     redirect_to car_path(@car)
+  #   else
+  #     render "cars/show", status: :unprocessable_entity
+  #   end
+  # end
 
   private
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,14 @@ class User < ApplicationRecord
         :recoverable, :rememberable, :validatable
   has_many :cars
   has_many :bookings
+  has_many :reviews, through: :bookings
 
   validates :first_name, presence: true
   validates :last_name, presence: true
   # validates :email, presence: true, uniqueness: true
   validates :address, presence: true
+
+  def wrote_review_for?(booking)
+    reviews.any? { |review| review.booking == booking }
+  end
 end
