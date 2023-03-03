6d270128e6bf5a8bad7070ac2602d5b560b1b530
Lena Mueller <lenakristinmueller@gmail.com>
2023-03-03 15:49:07 +0100

    show page update

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 require "open-uri"
+Review.destroy_all
 Booking.destroy_all
 Car.destroy_all
 User.destroy_all
